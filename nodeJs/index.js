d2061a9c7e6474ba7b57d1d137b3f2d68c8b6d36
Server <november14giel@gmail.com>
2018-06-18 16:52:25 -0400

    employee controller folder

--- nodeJs/index.js
+++ nodeJs/index.js
@@ -1,8 +1,11 @@
 //package import
 const express = require('express');
 const bodyParser = require('body-parser');
+
 //local import
 const { mongoose } = require('./db'); // this import establishes connection to mongodb using es6 destructuring method
+// request for the controller statement 
+let employeeController = require('./controller/employeeController');
 const PORT = process.env.PORT || 3000;
 //to work with express function, we call express using
 //👇
