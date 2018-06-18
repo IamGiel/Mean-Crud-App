f3205521f5c81ebef9daca987fac16a175626449
Server <november14giel@gmail.com>
2018-06-18 17:00:46 -0400

    define route on indexjs

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Mean-Crud-App
-Implement CRUD Operations in a MEAN Stack Application from Scratch. Here tech stack inlcude MongoDB, Express,Node.JS and Angular 5 for the application development.
+Implement CRUD Operations in a MEAN Stack Application from Scratch. Here, tech stack inlcude MongoDB, Express,Node.JS and Angular 5 for the application development.
 
 🖌 up on Angular 
+
+WorkFlow:
+1. Start the server environment,
+2. Set-up the back-end database environment MongoDB
+3. Define the routes that queries the Database
+
+
 <a href="https://www.youtube.com/watch?v=UYh6EvpQquw">...src</a>
--- nodeJs/index.js
+++ nodeJs/index.js
@@ -6,11 +6,17 @@ const bodyParser = require('body-parser');
 const { mongoose } = require('./db'); // this import establishes connection to mongodb using es6 destructuring method
 // request for the controller statement 
 let employeeController = require('./controller/employeeController');
+
+//define the port
 const PORT = process.env.PORT || 3000;
 //to work with express function, we call express using
 //👇
 const app = express();
 app.use(bodyParser.json())
+
+//to add the router from controller to this application
+//we add this middleware 👇🏼
+app.use("router", )
 
 //to start the express server
 //👇
