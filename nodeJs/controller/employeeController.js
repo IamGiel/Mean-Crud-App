3ad26af1e27237f050466248825a27c0128f88ee
Server <november14giel@gmail.com>
2018-06-18 17:52:44 -0400

    added a test data

--- nodeJs/controller/employeeController.js
+++ nodeJs/controller/employeeController.js
@@ -4,12 +4,26 @@ let router = express.Router() //👈🏼 Router from express
 
 let { Employee } = require('../models/employee')
 // adding a router that will retrieve all employees from the db
-//👇🏼 localhost:3000/Employees/list
+//👇🏼 http://localhost:3000/employees/list
 router.get('/list', (req, res)=> {
   Employee.find((err, docs)=>{
     if(!err){ res.send(docs)} 
     else console.log("Error in retrieving Employee Data: " + JSON.stringify(err, undefined, 2));
   });
+})
+
+// add our POST request to add data to our database
+router.post('/list', (req, res)=> {
+  //we need to create an Employee model class and save it in a var called `emp` 
+  //this gives us a new json data of the new employee using `req.body` object
+  let emp = new Employee({
+    name: req.body.name,
+    position: req.body.position,
+    office: req.body.office,
+    salary: req.body.salary,
+  });
+  //then save it to db
+  emp.save();
 })
 
 //we need to export this module
