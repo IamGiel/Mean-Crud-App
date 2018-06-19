e895d86a807a8fdffea20c2a28b3cecf22a562d4
Server <november14giel@gmail.com>
2018-06-18 23:41:44 -0400

    add get routes for specific id

--- nodeJs/controller/employeeController.js
+++ nodeJs/controller/employeeController.js
@@ -1,6 +1,8 @@
+
 //implement router from express
 const express = require('express');
 let router = express.Router() //👈🏼 Router from express
+let ObjectId = require('mongoose').Types.ObjectId; //mongoose objectId, helps us check if an _id is valid
 
 let { Employee } = require('../models/employee')
 // adding a router that will retrieve all employees from the db
@@ -11,6 +13,17 @@ router.get('/list', (req, res)=> {
     else console.log("Error in retrieving Employee Data: " + JSON.stringify(err, undefined, 2));
   });
 })
+
+// get request to specific user id
+router.get("/list/:id", (req, res) => {
+  //check if the id is valid
+  if(!ObjectId.isValid(req.params.id))
+    return res.status(400).send('No Record with given id : '`${req.params.id}`);
+  Employee.findById(req.params.id, (err, docs)=> {
+    if(!err) {res.send(docs)}
+    else console.log("Error in retrieving Employee Data: " + JSON.stringify(err, undefined, 2));
+  })
+});
 
 // add our POST request to add data to our database
 router.post('/list', (req, res)=> {
@@ -23,8 +36,15 @@ router.post('/list', (req, res)=> {
     salary: req.body.salary,
   });
   //then save it to db
-  emp.save();
+  emp.save((err, docs)=> {
+    if(!err){res.send(docs)}
+    else console.log("Error in retrieving Employee Data: " + JSON.stringify(err, undefined, 2));
+  });
+
+  //we will use `postman` to insert and test data
 })
+
+
 
 //we need to export this module
 module.exports = router;
