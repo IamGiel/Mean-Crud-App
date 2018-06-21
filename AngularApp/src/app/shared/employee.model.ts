b00b5a38794b7ef98bfaacea4be6ef9c1af198b9
Server <november14giel@gmail.com>
2018-06-21 00:38:56 -0400

    fixing cors

--- AngularApp/src/app/shared/employee.model.ts
+++ AngularApp/src/app/shared/employee.model.ts
@@ -1,9 +1,9 @@
 // copy the key value pairs produced by mongoDB
 // change comma to semi-colon at the end of each line
 export class Employee {
-  _id: number;
+  _id: any;
   name: string;
   position: string
   office: string;
-  salary: number;
+  salary: any;
 }
